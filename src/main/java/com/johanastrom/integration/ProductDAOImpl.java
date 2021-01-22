c085aae843dba016488b42cd859f9e71d52cf930
Johan Astrom <johan.astrom89@gmail.com>
2021-01-22 18:23:08 +0100

    Rudimental ProductDAOImpl added

--- src/main/java/com/johanastrom/integration/ProductDAOImpl.java
+++ src/main/java/com/johanastrom/integration/ProductDAOImpl.java
@@ -1,4 +1,36 @@
 package com.johanastrom.integration;
 
-public class ProductDAOImpl {
+import com.johanastrom.company.Product;
+import com.johanastrom.company.ProductCategory;
+import com.johanastrom.company.Supplier;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class ProductDAOImpl implements ProductDAO {
+
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-relations");
+    EntityManager em;
+
+    @Override
+    public void add(Product p) {
+
+    }
+
+    @Override
+    public float incrementUnitPrice(float incrementValue) {
+        return 0;
+    }
+
+    @Override
+    public Supplier getSupplier(Product p) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getByCategory(ProductCategory pc) {
+        return null;
+    }
 }
