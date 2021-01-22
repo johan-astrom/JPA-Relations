a9cead7b54a3b3431579bc0f8973d85ff8120bca
Johan Astrom <johan.astrom89@gmail.com>
2021-01-22 18:06:35 +0100

    Added Supplier entity

--- src/main/java/com/johanastrom/company/Supplier.java
+++ src/main/java/com/johanastrom/company/Supplier.java
@@ -1,4 +1,41 @@
 package com.johanastrom.company;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "everyloop.company.suppliers")
 public class Supplier {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String companyName;
+    private String contactName;
+    private String address;
+    private String city;
+    private String country;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
 }
